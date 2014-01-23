8301e7c9495c2fa422f269463666fb28bf98007d
Max Angerbauer <angerbauer@wienfluss.net>
2014-01-23 14:06:02 +0100

    added colander activity schema for (de)serialization + first test.

---
+++ sandglass/time/forms/activity.py
@@ -0,0 +1,34 @@
+from colander import DateTime
+from colander import Integer
+from colander import Length
+from colander import OneOf
+from colander import SchemaNode
+from colander import String
+from colander import SequenceSchema
+
+from sandglass.time.forms import BaseModelSchema
+from sandglass.time.models.activity import ACTIVITY_TYPES
+from sandglass.time.models.activity import ACTIVITY_UNASSIGNED
+
+
+class ActivitySchema(BaseModelSchema):
+    """
+    Schema definition for activity model.
+
+    TODO: define required, missing, default attributes.
+    """
+    description = SchemaNode(String())
+    start = SchemaNode(DateTime()) # TODO: default, TZ aware
+    end = SchemaNode(DateTime()) # TODO: default, TZ aware
+    activity_type = SchemaNode(String(),
+                               validator=OneOf(ACTIVITY_TYPES.keys()),
+                               default=ACTIVITY_UNASSIGNED)
+    project_id = SchemaNode(Integer())
+    task_id = SchemaNode(Integer())
+
+
+class ActivityListSchema(SequenceSchema):
+    """
+    Schema definition for a list of activities.
+    """
+    activity = ActivitySchema()
