b59ca810f1905559a78951fa87622a1a8fc70e76
Jer?nimo Albi <albi@wienfluss.net>
2014-01-24 12:18:33 +0100

    Improved resource API initialization and URL mappings.

--- sandglass/time/directives.py
+++ sandglass/time/directives.py
@@ -1,81 +1,76 @@
-ALL_REQ_METHODS = ('GET', 'POST', 'PUT', 'DELETE')
+from pyramid.path import DottedNameResolver
 
 
-def add_api_resource(config, cls):
+def add_rest_resource(config, cls_or_dotted):
     """
-    TODO
+    Add routes and views for a `RestResource` class.
 
     """
+    # Get the resource class definition
+    resolver = DottedNameResolver()
+    cls = resolver.maybe_resolve(cls_or_dotted)
+
     resource_name = cls.get_route_prefix()
     resource_prefix = '/{}/'.format(resource_name)
+    resource_data = (
+        # /route_prefix/
+        #    GET: List all items
+        #    POST: Create new item(s)
+        #    DELETE: Delete all items
+        {'path': resource_prefix,
+         'name_suffix': '_all',
+         'methods': ('GET', 'POST', 'DELETE'),
+         'enable_rpc': True, },
+        # /route_prefix/{pk}/
+        #    GET: Get a single item
+        #    PUT: Update a single item
+        #    DELETE: Delete a single item
+        {'path': (resource_prefix + '{pk}/'),
+         'name_suffix': '',
+         'methods': ('GET', 'PUT', 'DELETE'),
+         'enable_rpc': True, },
+        # /route_prefix/{pk}/{related_name}/
+        #    GET: List all related items
+        #    POST: Create related item(s)
+        #    PUT: Update related item(s)
+        #    DELETE: Delete related item(s)
+        {'path': (resource_prefix + '{pk}/{related_name}/'),
+         'name_suffix': '_related',
+         'methods': ('GET', 'POST', 'PUT', 'DELETE'),
+         'enable_rpc': False, },
+    )
 
-    # /route_prefix/
-    #     GET: list all items
-    #     POST: Create new items
-    #     DELETE: Delete all items
-    config.add_route(resource_name + '_multi',
-        pattern=resource_prefix,
-        request_method=('GET', 'POST', 'DELETE'))
-    config.add_view(cls,
-        attr='get_all',
-        route_name=(resource_name + '_multi'),
-        renderer='json',
-        request_method='GET')
-    # config.add_view(cls,
-    #     attr='post_all',
-    #     route_name='',
-    #     request_method='POST')
-    # config.add_view(cls,
-    #     attr='delete_all',
-    #     route_name='',
-    #     request_method='DELETE')
+    # Name of the entry point method for all RPC calls
+    rpc_attr_name = 'handle_rpc_call'
+    # Generate routes and attach views for current class
+    for data in resource_data:
+        route_name = resource_name + data['name_suffix']
+        request_methods = data['methods']
+        # Create a new URL route
+        config.add_route(route_name,
+                         pattern=data['path'],
+                         request_method=request_methods)
+        # Add views to handle different request methods in this view
+        for method in request_methods:
+            # Init the name of the methos that the class should
+            # implement to handle requests for current request method
+            attr_name = '{0}{1}'.format(method.lower(), data['name_suffix'])
+            view_handler = getattr(cls, attr_name, None)
+            # Skip non callable attributes
+            if not hasattr(view_handler, '__call__'):
+                continue
 
-    # /route_prefix/{pk}/
-    #     GET: get single item
-    #     PUT: update single item
-    #     DELETE: delete single item
-    # config.add_route(resource_name + '_single',
-    #     pattern=(resource_prefix + '{pk}/'),
-    #     request_method=('GET', 'PUT', 'DELETE'))
-    # config.add_view(cls,
-    #     attr='get',
-    #     route_name=(resource_name + '_single'),
-    #     request_method='GET')
-    # config.add_view(cls,
-    #     attr='put',
-    #     route_name='',
-    #     request_method='PUT')
-    # config.add_view(cls,
-    #     attr='delete',
-    #     route_name='',
-    #     request_method='DELETE')
-    # config.add_view(cls,
-    #     attr='call_method',
-    #     route_name='',
-    #     request_param='call',
-    #     request_method=('GET', 'POST'))
+            config.add_view(cls,
+                            attr=attr_name,
+                            route_name=route_name,
+                            renderer='json',
+                            request_method=method)
 
-    # /route_prefix/{pk}/{related_name}/
-    #     GET: list all related items
-    #     POST: create related item(s)
-    #     PUT: update related item(s)
-    #     DELETE: delete related item(s)
-    # config.add_route('',
-    #     pattern=(route_prefix + '{pk}/{related_name}/'),
-    #     request_method=ALL_REQ_METHODS)
-    # config.add_view(cls,
-    #     attr='get_all_related',
-    #     route_name='',
-    #     request_method='GET')
-    # config.add_view(cls,
-    #     attr='post_all_related',
-    #     route_name='',
-    #     request_method='POST')
-    # config.add_view(cls,
-    #     attr='put_all_related',
-    #     route_name='',
-    #     request_method='PUT')
-    # config.add_view(cls,
-    #     attr='delete_all_related',
-    #     route_name='',
-    #     request_method='DELETE')
+        # Attach a view to handle RPC function calls
+        rpc_view_handler = getattr(cls, rpc_attr_name, None)
+        if data['enable_rpc'] and hasattr(rpc_view_handler, '__call__'):
+            config.add_view(cls,
+                            attr=rpc_attr_name,
+                            route_name=route_name,
+                            request_param='call',
+                            request_method=('GET', 'POST'))
