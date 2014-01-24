305ef5f2a06390ec48886399f62bcc52d684415f
Max Angerbauer <angerbauer@wienfluss.net>
2014-01-24 15:53:57 +0100

    adder user schema; started user resource post_all;

--- sandglass/time/api/v1/user.py
+++ sandglass/time/api/v1/user.py
@@ -1,5 +1,7 @@
 from sandglass.time.api.model import ModelResource
 from sandglass.time.models.user import User
+from sandglass.time.forms.user import UserSchema
+from sandglass.time.forms.user import Users
 
 
 class UserResource(ModelResource):
@@ -9,3 +11,14 @@ class UserResource(ModelResource):
     """
     name = 'users'
     model = User
+    schema = UserSchema
+    # list_schema = 'sandglass.time.forms.user.UserListSchema'
+    list_schema = Users
+
+    def post_all(self):
+        # TODO
+        cstruct = self.request.json_body
+        deserialized = self.list_schema().deserialize(cstruct)
+        print deserialized
+
+        return
