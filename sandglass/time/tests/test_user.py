e8cf6889dac40763ddf08262ccdb558242f62eee
Florian Cech <lyrixderaven@gmail.com>
2014-01-28 14:24:30 +0100

    Added Functional Test for User model (Skeleton)

---
+++ sandglass/time/tests/test_user.py
@@ -0,0 +1,105 @@
+import sandglass.time
+
+# TODO Improve Tests: Tests for multiple deletion, multiple getting
+
+class UserTest(sandglass.time.tests.BaseFunctionalTest):
+
+    def test_user_create(self):
+
+        # Create a user
+        userlist = []
+        userlist.append({
+            "email": "timeywimey@wienfluss.net",
+            "first_name": "Dr",
+            "last_name": "Who"
+        })
+
+        create_response = self.testapp.post_json('/time/api/v1/users/',
+                                                 userlist,
+                                                 status=200)
+        created_id = create_response.json[0]['id']
+        json = create_response.json
+
+
+        self.failUnless(created_id.__class__ == int)
+
+        self.assertTrue(json[0]['first_name'] == 'Dr',
+                        'First name should have been "Dr"')
+        self.assertTrue(json[0]['last_name'] == 'Who',
+                        'Last name should have been "Who"')
+        self.assertTrue(json[0]['email'] == 'timeywimey@wienfluss.net',
+                        'Email should have been "timeywimey@wienfluss.net"')
+
+    def test_user_create_multiple(self):
+
+        # Create a user
+        userlist = []
+        userlist.append({
+            "email": "humpdydumpdy@wienfluss.net",
+            "first_name": "James William",
+            "last_name": "Elliot"
+        })
+        userlist.append({
+            "email": "ruggedlyhandsome@wienfluss.net",
+            "first_name": "Rick",
+            "last_name": "Castle"
+        })
+        
+        create_response = self.testapp.post_json('/time/api/v1/users/',
+                                                 userlist,
+                                                 status=200)
+        json = create_response.json
+        self.failUnless(len(json) == 2,
+                        'Not enough entries in response, expected 2')
+
+        self.assertTrue(json[0]['first_name'] == 'James William',
+                        'First name should have been "James William"')
+        self.assertTrue(json[0]['last_name'] == 'Elliot',
+                        'Last name should have been "Elliot"')
+        self.assertTrue(json[0]['email'] == 'humpdydumpdy@wienfluss.net',
+                        'Email should have been "humpdydumpdy@wienfluss.net"')
+
+        self.assertTrue(json[1]['first_name'] == 'Rick',
+                        'First name should have been "Rick')
+        self.assertTrue(json[1]['last_name'] == 'Castle',
+                        'Last name should have been "Castle"')
+        self.assertTrue(json[1]['email'] == 'ruggedlyhandsome@wienfluss.net',
+                        'Email should have been "ruggedlyhandsome@wienfluss.net"')
+
+    def test_user_delete(self):
+        # Create a second user
+        userlist = []
+        userlist.append({
+            "email": "wibblywobbly@wienfluss.net",
+            "first_name": "The",
+            "last_name": "Tardis"
+        })
+
+        create_response = self.testapp.post_json('/time/api/v1/users/',
+                                                 userlist,
+                                                 status=200)
+        created_id = create_response.json[0]['id']
+
+        # Delete that user again
+        delete_response = self.testapp.delete_json(
+            '/time/api/v1/users/{}/'.format(created_id), status=200)
+
+        self.testapp.get(
+            '/time/api/v1/users/{}/'.format(created_id), status=404)
+
+    def test_get_user(self):
+        # Create a third user
+        userlist = []
+        userlist.append({
+            "email": "ittybitty@wienfluss.net",
+            "first_name": "Amilia",
+            "last_name": "Pond"
+        })
+
+        create_response = self.testapp.post_json('/time/api/v1/users/',
+                                                 userlist,
+                                                 status=200)
+        created_id = create_response.json[0]['id']
+
+        get_response = self.testapp.get(
+            '/time/api/v1/users/{}/'.format(created_id), status=200)
