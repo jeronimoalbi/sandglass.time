71f8a09757107679e1e7758b9d36fa220bda35a9
Max Angerbauer <angerbauer@wienfluss.net>
2014-01-29 14:50:40 +0100

    added sandglass-test.ini file and BaseTest class. (includes fixtures but they don't work yet).

--- sandglass/time/tests/__init__.py
+++ sandglass/time/tests/__init__.py
@@ -1,8 +1,13 @@
+import os
 import unittest
+
+from paste.deploy import loadapp
+from paste.deploy.loadwsgi import appconfig
 
 from pyramid import testing
 from sandglass.time.main import run_wsgi
 from webtest import TestApp
+
 
 # TODO import these from an ini file
 TEST_SETTINGS = {"use": "egg:sandglass.time",
@@ -22,7 +27,77 @@ TEST_SETTINGS = {"use": "egg:sandglass.time",
                  "database.echo": "false",
                  }
 
+
+def get_config_file_path():
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    config_file_path = os.path.join(test_dir,
+                                    os.pardir, os.pardir, os.pardir,
+                                    'sandglass-test.ini')
+
+    return config_file_path
+
+
+settings = appconfig('config:' + get_config_file_path())
+
+
+from sandglass.time import models
+from sandglass.time.models import META
+from fixture import SQLAlchemyFixture
+from fixture.style import NamedDataStyle
+
+from sqlalchemy import engine_from_config
+
+engine = engine_from_config(settings, prefix='database.')
+
+from fixture import DataSet
+
+class UserData(DataSet):
+    class homer_simpson:
+        email = "homer@simpson.com"
+        first_name = "Homer"
+        last_name = "Simpson"
+    class marge_simpson:
+        email = "marge@simpson.com"
+        first_name = "marge"
+        last_name = "Simpson"
+
+
+dbfixture = SQLAlchemyFixture(
+    env={'UserData': models.user.User, },
+    engine=engine,
+    style=NamedDataStyle()
+)
+
+
 # Test Base Classes
+class BaseTest(unittest.TestCase):
+    """
+    Base class for functional test
+
+    Creates testapp instance by wrapping main.run_wsgi()
+    """
+    def setUp(self):
+        # self.app = run_wsgi(TEST_SETTINGS, **TEST_SETTINGS)
+        # 
+        # self.testapp = TestApp(self.app)
+        print "Setting settings..."
+        self.settings = settings
+        print "META create_all..."
+        META.create_all(engine)
+
+        self.data = dbfixture.data(UserData)
+        self.data.setup()
+
+        print "wsgiapp loadapp with config..."
+        wsgiapp = loadapp('config:%s' % get_config_file_path())
+
+        print "TestApp..."
+        self.testapp = TestApp(wsgiapp)
+
+    def tearDown(self):
+        self.data.teardown()
+        print "META drop_all..."
+        META.drop_all(engine)
 
 
 class BaseFunctionalTest(unittest.TestCase):
