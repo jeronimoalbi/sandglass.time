88f87da27788b4c894648b5f7e9cd2c327a00452
Jer?nimo Albi <albi@wienfluss.net>
2014-02-03 11:38:03 +0100

    Added get user by credentials (email or key value).

--- sandglass/time/api/v1/user.py
+++ sandglass/time/api/v1/user.py
@@ -1,3 +1,6 @@
+from pyramid.exceptions import NotFound
+
+from sandglass.time.api import rpc
 from sandglass.time.api.model import ModelResource
 from sandglass.time.forms.user import UserListSchema
 from sandglass.time.forms.user import UserSchema
@@ -13,3 +16,29 @@ class UserResource(ModelResource):
     model = User
     schema = UserSchema
     list_schema = UserListSchema
+
+    @rpc(method='get')
+    def get_by_credential(self):
+        """
+        Get a User by email or key.
+
+        Return a User or raise HTTP 404.
+
+        """
+        email = self.request.GET.get('email')
+        key = self.request.GET.get('key')
+        filters = {}
+        if email:
+            filters['email'] = email
+        elif key:
+            filters['key'] = key
+        else:
+            user = None
+
+        if filters:
+            user = User.query().filter_by(**filters).first()
+
+        if not user:
+            raise NotFound()
+
+        return user
