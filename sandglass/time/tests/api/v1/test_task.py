d2d32462307c574f94eeb0d172dada7701d72cc6
Jerónimo Albi <jeronimoalbi@users.noreply.github.com>
2014-03-01 22:44:43 +0100

    Unittest changes, minor fixes, task resource functional tests. Fixes #9.
    * Wrote some functional tests for task resource
    * Fixed unittests to work with fixtures when a test modifies fixture inserted data
    * Refactored project include and setup functions to be more modular
    * Moved project include and setup functions to a `config` module
    * Fixed related URL path generation method

--- sandglass/time/tests/api/v1/test_task.py
+++ sandglass/time/tests/api/v1/test_task.py
@@ -1,7 +1,9 @@
 from sandglass.time.tests import FunctionalTestCase
+from sandglass.time.api.v1.task import TaskResource
 from sandglass.time.tests import fixture
 
 from .fixtures import TaskData
+from .fixtures import ProjectData
 
 
 class TaskResourceTest(FunctionalTestCase):
@@ -12,11 +14,33 @@ class TaskResourceTest(FunctionalTestCase):
     # Use authentication for each request by default
     require_authorization = True
 
+    def get_task_data(self):
+        user = self.fixture_data['UserData']['DrWho']
+        project = self.fixture_data['ProjectData']['PublicProject']
+        return {
+            'name': u"Test task",
+            'user_id': user['id'],
+            'project_id': project['id'],
+        }
+
+    @fixture(ProjectData)
     def test_create_single_task(self):
         """
         Create a single task using the API.
 
         """
+        task_data = self.get_task_data()
+        url = TaskResource.get_collection_path()
+        # Create the new task
+        response = self.post_json(url, [task_data])
+        # Check for success, and that body contains a list with one element
+        self.assertTrue(response.status_int, 200)
+        self.assertTrue(isinstance(response.json_body, list))
+        self.assertEqual(len(response.json_body), 1)
+        # Check that task has a valid ID value
+        task = response.json_body[0]
+        self.assertTrue('id' in task)
+        self.assertTrue(isinstance(task['id'], int))
 
     def test_update_single_task(self):
         """
@@ -36,3 +60,13 @@ class TaskResourceTest(FunctionalTestCase):
         Test a task with a projects.
 
         """
+        task = self.fixture_data['TaskData']['Backend']
+        url = TaskResource.get_related_path(task['id'], 'project')
+        # Get task project data
+        response = self.get_json(url)
+        self.assertTrue(response.status_int, 200)
+        self.assertTrue(isinstance(response.json_body, dict))
+
+        # Check that project is the same as the requested task data
+        project = response.json_body
+        self.assertEqual(task.project.id, project['id'])
