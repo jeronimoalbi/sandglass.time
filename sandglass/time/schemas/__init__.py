b1631f38aad6c96a24b97d16f5ef6f69bd8b1c6d
Jerónimo Albi <jeronimoalbi@users.noreply.github.com>
2014-03-06 11:08:48 +0100

    Modified user schema to support dictionary objects in `data` field (fixes #12).

--- sandglass/time/schemas/__init__.py
+++ sandglass/time/schemas/__init__.py
@@ -1,6 +1,8 @@
 from colander import Integer
+from colander import Invalid
 from colander import drop
 from colander import MappingSchema
+from colander import null
 from colander import SchemaNode
 from colander import SequenceSchema
 
@@ -19,3 +21,32 @@ class IdListSchema(SequenceSchema):
 
     """
     id = SchemaNode(Integer())
+
+
+class Dictionary(object):
+    """
+    Schema type for Python dictionary fields.
+
+    """
+    def serialize(self, node, appstruct):
+        if appstruct is null or appstruct is None:
+            return null
+
+        if not isinstance(appstruct, dict):
+            raise Invalid(node, '%r is not a dictionary' % appstruct)
+
+        # Return given dictionary (don't serialize)
+        return appstruct
+
+    def deserialize(self, node, cstruct):
+        if cstruct is null or cstruct is None:
+            return null
+
+        if not isinstance(cstruct, dict):
+            raise Invalid(node, '%r is not a dictionary' % cstruct)
+
+        # Return given dictionary (don't deserialize)
+        return cstruct
+
+    def cstruct_children(self, node, cstruct):
+        return []
